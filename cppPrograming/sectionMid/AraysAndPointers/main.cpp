b07394d618628c2fdd64fba3a31add1380179d76
luczod <kzod5611@gmail.com>
2024-12-04 13:54:13 -0300

    feat: add left-shift and right-shift operators

--- cppPrograming/sectionMid/AraysAndPointers/main.cpp
+++ cppPrograming/sectionMid/AraysAndPointers/main.cpp
@@ -2,8 +2,38 @@
 
 using namespace std;
 
-int main() {
-    
+void example_one() {
+  // Create an int variable
+  int myInt;
+  
+  int myNumbers[4] = {25, 50, 75, 100};
+  
+  // Get the memory size in bytes of an int
+  printf("%lu\n", sizeof(myInt));
+  
+  // Get the size of the myNumbers array
+  printf("%lu\n", sizeof(myNumbers));
+  
+  // Get the value of the first element in myNumbers
+  // myNumbers is a pointer to the first element in myNumbers
+  printf("%d\n", *myNumbers);
+  
+  // Get the value of the second element in myNumbers
+  printf("%d\n", *(myNumbers + 1));
+  
+   
+  // Shifts the value to left by the number of bits specified by the right operand.
+  // (a<<b) is equivalent to multiplying a with 2^b (2 raised to power b). 
+  printf("%d\n", *myNumbers << 3);
+  
+  // Shifts the value to right by the number of bits specified by the right operand.
+  // (a>>b)  is equivalent to dividing a with 2^b (2 raised to power b). 
+  printf("%d\n", *myNumbers >> 2);
+
+  // The left-shift and right-shift operators should not be used for negative numbers. 
+}
+
+void example_two() {
     int scores[] {100, 95, 89};
     
     cout << "Value of scores: " << scores << endl;     
@@ -32,6 +62,13 @@ int main() {
     cout << *(scores +2) << endl;
     
     cout << endl;
+
+}
+
+int main() {
+    example_one();
+    example_two();
+  
     return 0;
 }
 
